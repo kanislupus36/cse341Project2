e3cd14766d8fa26aab84c62773b080b8ef7457be
Kadin <kanislupus36@protonmail.com>
2025-03-31 19:46:59 -0600

    week 4 assignment

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const passport = require('passport');
 const controller = require('../controllers/index');
+const { isAuthenticated } = require("../middleware/authenticate");
 
 router.use('/', require('./swagger'));
 
@@ -7,11 +9,19 @@ router.get('/books', controller.getAllBooks);
 router.get('/games', controller.getAllGames);
 router.get('/books/:id', controller.getSingleBook);
 router.get('/games/:id', controller.getSingleGame);
-router.post('/books', controller.createBook);
-router.post('/games', controller.createGame);
-router.put('/books/:id', controller.updateBook);
-router.put('/games/:id', controller.updateGame);
-router.delete('/books/:id', controller.deleteBook);
-router.delete('/games/:id', controller.deleteGame);
+router.post('/books', isAuthenticated, controller.createBook);
+router.post('/games', isAuthenticated, controller.createGame);
+router.put('/books/:id', isAuthenticated, controller.updateBook);
+router.put('/games/:id', isAuthenticated, controller.updateGame);
+router.delete('/books/:id', isAuthenticated, controller.deleteBook);
+router.delete('/games/:id', isAuthenticated, controller.deleteGame);
+
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) {return next(err); }
+        res.redirect('/');
+    });
+});
 
 module.exports = router;
